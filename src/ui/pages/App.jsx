6987a3d251274feeef47ebb9f3d818041a37b25b
Karim <kfmust@gmail.com>
2024-10-27 16:16:58 +0300

    Lesson Schedule UI

--- src/ui/pages/App.jsx
+++ src/ui/pages/App.jsx
@@ -1,7 +1,51 @@
 import "./App.css";
+import LessonScheduleView from "../views/lessonScheduleView/LessonScheduleView";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [lessonSchedule, setLessonSchedule] = useState({
+    weekday: 1,
+    lessons: [
+      {
+        lessons: [
+          {
+            name: "тест",
+            classroom: "202",
+          },
+        ],
+        class: "12Я",
+      },
+      {
+        lessons: [
+          {
+            name: "тест",
+            classroom: "202",
+          },
+        ],
+        class: "12Я",
+      },
+      {
+        lessons: [
+          {
+            name: "тест",
+            classroom: "202",
+          },
+        ],
+        class: "12Я",
+      },
+      {
+        lessons: [
+          {
+            name: "тест",
+            classroom: "202",
+          },
+        ],
+        class: "12Я",
+      },
+    ],
+  });
+
+  return <>{<LessonScheduleView lessonSchedule={lessonSchedule} />}</>;
 }
 
 export default App;
---
+++ src/ui/views/lessonScheduleView/LessonScheduleView.jsx
@@ -0,0 +1,56 @@
+import "./LessonScheduleView.css";
+
+// View с расписанием уроков
+function LessonScheduleView({ lessonSchedule }) {
+  console.log(lessonSchedule);
+  // Максимальное количество уроков у всех классов
+  const maxLessons = Math.max(
+    ...lessonSchedule.lessons.map((cls) => cls.lessons.length)
+  );
+
+  return (
+    <table className="lessonScheduleTable">
+      <thead>
+        <tr>
+          <th>
+            <p className="sectionTitleCell">Урок</p>
+          </th>
+          {lessonSchedule.lessons.map((cls) => (
+            <th key={cls.class}>
+              <p className="sectionTitleCell">{cls.class}</p>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Array.from({ length: maxLessons }).map((_, index) => (
+          <tr
+            key={index}
+            style={{
+              backgroundColor:
+                index % 2 === 0
+                  ? "var(--secondary-background-color)"
+                  : "transparent",
+            }}
+          >
+            <td>
+              <p className="sectionTitleCell">{index + 1}</p>
+            </td>
+            {lessonSchedule.lessons.map((cls) => (
+              <td key={cls.class + index}>
+                {cls.lessons[index] ? (
+                  <div className="lessonCell">
+                    <p className="lessonTitle">{cls.lessons[index].name}</p>
+                    <p className="classroom">{cls.lessons[index].classroom}</p>
+                  </div>
+                ) : null}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default LessonScheduleView;
